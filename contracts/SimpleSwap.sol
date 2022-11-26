6e6ed365d9968e395474e41cd99390aa1cbfcd8d
Pao Chueh <pao.chueh@theomnieyes.com>
2022-11-26 23:19:06 +0800

    Fix issues

--- contracts/SimpleSwap.sol
+++ contracts/SimpleSwap.sol
@@ -4,11 +4,11 @@ pragma solidity 0.8.17;
 import {ISimpleSwap} from "./interface/ISimpleSwap.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
-import {SafeMath} from "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import {Address} from "@openzeppelin/contracts/utils/Address.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-contract SimpleSwap is ISimpleSwap, ERC20 {
-    using SafeMath for uint256;
+contract SimpleSwap is ISimpleSwap, ERC20, ReentrancyGuard {
 
     address public tokenA;
     address public tokenB;
@@ -37,16 +37,12 @@ contract SimpleSwap is ISimpleSwap, ERC20 {
         tokenB = _tokenB;
     }
 
-    /// @notice Swap tokenIn for tokenOut with amountIn
-    /// @param tokenIn The address of the token to swap from
-    /// @param tokenOut The address of the token to swap to
-    /// @param amountIn The amount of tokenIn to swap
-    /// @return amountOut The amount of tokenOut received
+    /// @inheritdoc ISimpleSwap
     function swap(
         address tokenIn,
         address tokenOut,
         uint256 amountIn
-    ) external override returns (uint256 amountOut) {
+    ) external nonReentrant override returns (uint256 amountOut) {
         require(tokenIn == tokenA || tokenIn == tokenB, "SimpleSwap: INVALID_TOKEN_IN");
         require(tokenOut == tokenA || tokenOut == tokenB, "SimpleSwap: INVALID_TOKEN_OUT");
         require(tokenIn != tokenOut, "SimpleSwap: IDENTICAL_ADDRESS");
@@ -57,13 +53,13 @@ contract SimpleSwap is ISimpleSwap, ERC20 {
         uint256 reserveTokenOut = ERC20(tokenOut).balanceOf(address(this));
 
         // calculate amountOut
-        uint256 totalTokenIn = reserveTokenIn.add(amountIn);
-        amountOut = ((totalTokenIn.mul(reserveTokenOut)).sub(_kValue)).div(totalTokenIn);
+        uint256 totalTokenIn = reserveTokenIn + amountIn;
+        amountOut = ((totalTokenIn * reserveTokenOut) -_kValue)/(totalTokenIn);
 
         require(amountOut > 0, "SimpleSwap: INSUFFICIENT_OUTPUT_AMOUNT");
 
         // get tokenIn from msg.sender
-        ERC20(tokenIn).transferFrom(msg.sender, address(this), amountIn);
+        SafeERC20.safeTransferFrom(ERC20(tokenIn), msg.sender, address(this), amountIn);
 
         // transfer tokenOut to msg.sender
         ERC20(tokenOut).transfer(msg.sender, amountOut);
@@ -76,14 +72,10 @@ contract SimpleSwap is ISimpleSwap, ERC20 {
         emit Swap(msg.sender, tokenIn, tokenOut, amountIn, amountOut);
     }
 
-    /// @notice Add liquidity to the pool
-    /// @param amountAIn The amount of tokenA to add
-    /// @param amountBIn The amount of tokenB to add
-    /// @return amountA The actually amount of tokenA added
-    /// @return amountB The actually amount of tokenB added
-    /// @return liquidity The amount of liquidity minted
+    /// @inheritdoc ISimpleSwap
     function addLiquidity(uint256 amountAIn, uint256 amountBIn)
         external
+        nonReentrant
         override
         returns (
             uint256 amountA,
@@ -101,18 +93,18 @@ contract SimpleSwap is ISimpleSwap, ERC20 {
 
         // if pool is empty, that's mean is first addLiquidity user
         if (totalSupply == 0) {
-            liquidity = Math.sqrt(amountAIn.mul(amountBIn)); // (amountA x amountB)^2
+            liquidity = Math.sqrt(amountAIn * amountBIn); // (amountA x amountB)^2
             amountA = amountAIn;
             amountB = amountBIn;
         } else {
             // if pool is not empty
             // choose min one
             liquidity = Math.min(
-                (amountAIn.mul(totalSupply)).div(_reserveA),
-                (amountBIn.mul(totalSupply)).div(_reserveB)
+                (amountAIn*totalSupply)/_reserveA,
+                (amountBIn*totalSupply)/_reserveB
             );
-            amountA = (liquidity.mul(_reserveA)).div(totalSupply);
-            amountB = (liquidity.mul(_reserveB)).div(totalSupply);
+            amountA = (liquidity*_reserveA)/totalSupply;
+            amountB = (liquidity*_reserveB)/totalSupply;
         }
 
         require(liquidity > 0, "SimpleSwap: INSUFFICIENT_LIQUIDITY_MINTED");
@@ -121,8 +113,8 @@ contract SimpleSwap is ISimpleSwap, ERC20 {
         _updateReservesAndK(true, amountA, amountB);
 
         // get the tokens from msg.sender
-        ERC20(tokenA).transferFrom(msg.sender, address(this), amountA);
-        ERC20(tokenB).transferFrom(msg.sender, address(this), amountB);
+        SafeERC20.safeTransferFrom(ERC20(tokenA), msg.sender, address(this), amountA);
+        SafeERC20.safeTransferFrom(ERC20(tokenB), msg.sender, address(this), amountB);
 
         // mint LP token to msg.sender
         _mint(msg.sender, liquidity);
@@ -131,12 +123,10 @@ contract SimpleSwap is ISimpleSwap, ERC20 {
         emit AddLiquidity(msg.sender, amountA, amountB, liquidity);
     }
 
-    /// @notice Remove liquidity from the pool
-    /// @param liquidity The amount of liquidity to remove
-    /// @return amountA The amount of tokenA received
-    /// @return amountB The amount of tokenB received
+    /// @inheritdoc ISimpleSwap
     function removeLiquidity(uint256 liquidity)
         external
+        nonReentrant
         override
         returns (uint256 amountA, uint256 amountB)
     {
@@ -145,14 +135,14 @@ contract SimpleSwap is ISimpleSwap, ERC20 {
         // reference UniswapV2Pair.sol:burn
         // calculate token's amount from current liquidity
         uint256 totalSupply = totalSupply();
-        amountA = (liquidity.mul(_reserveA)).div(totalSupply);
-        amountB = (liquidity.mul(_reserveB)).div(totalSupply);
+        amountA = (liquidity*_reserveA)/totalSupply;
+        amountB = (liquidity*_reserveB)/totalSupply;
 
         // update reserves and K value
         _updateReservesAndK(false, amountA, amountB);
 
         // send LP token to contract
-        transfer(address(this), liquidity);
+        _transfer(msg.sender, address(this), liquidity);
 
         // transfer back tokens to msg.sender
         ERC20(tokenA).transfer(msg.sender, amountA);
@@ -165,21 +155,17 @@ contract SimpleSwap is ISimpleSwap, ERC20 {
         emit RemoveLiquidity(msg.sender, amountA, amountB, liquidity);
     }
 
-    /// @notice Get the reserves of the pool
-    /// @return _reserveA The reserve of tokenA
-    /// @return _reserveB The reserve of tokenB
+    /// @inheritdoc ISimpleSwap
     function getReserves() external view override returns (uint256, uint256) {
         return (_reserveA, _reserveB);
     }
 
-    /// @notice Get the address of tokenA
-    /// @return tokenA The address of tokenA
+    /// @inheritdoc ISimpleSwap
     function getTokenA() external view override returns (address) {
         return tokenA;
     }
 
-    /// @notice Get the address of tokenB
-    /// @return tokenB The address of tokenB
+    /// @inheritdoc ISimpleSwap
     function getTokenB() external view override returns (address) {
         return tokenB;
     }
@@ -196,6 +182,6 @@ contract SimpleSwap is ISimpleSwap, ERC20 {
             _reserveA -= amountA;
             _reserveB -= amountB;
         }
-        _kValue = _reserveA.mul(_reserveB);
+        _kValue = _reserveA * _reserveB;
     }
 }
